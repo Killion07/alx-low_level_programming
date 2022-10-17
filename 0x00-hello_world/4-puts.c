a474b12bee848f88ff1d30906f53298522ef12e0
Killion07 <wkilion@gmail.com>
2022-10-16 18:39:50 -0700

    Puts c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-
+/*
+*A command for printing a function using puts
+*Return value of 0
+*/
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
