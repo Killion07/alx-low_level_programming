cff596d32ff6cf21ec63b13a847085a5719333c6
Killion07 <wkilion@gmail.com>
2022-10-18 02:28:41 -0700

    6. Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Print numbers 1 to 10 using putchar
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0 ; n <= 10 ; n++)
+		putchar ((n % 10) + 0);
+	putchar('\n');
+
+	return (0);
+}
+
