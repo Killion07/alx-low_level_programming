239493ea9e789f8500eeb74aea2154f4c50fd92c
Killion07 <wkilion@gmail.com>
2022-10-19 11:05:04 -0700

    Is alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *_isalpha - checking whther a character is alphabet
+ *@c: Character to check for
+ *
+ * Return: 0 or 1
+ */
+int _isalpha(int c)
+{
+	if (_isalpha(c) == 0)
+		return (0);
+	else
+		return (1);
+}
