b6b744ea20e78bf71caca998fd6c3ae98b5afe2c
Killion07 <wkilion@gmail.com>
2022-10-18 20:01:11 -0700

    3rd assignments

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,11 +11,11 @@ void print_alphabet_x10(void)
 
 	while (j < 10)
 	{
-	for (i = 'a' ; i <= 'z' ; i++)
-	{
-		_putchar(i);
+		for (i = 'a' ; i <= 'z' ; i++)
+		{
+			_putchar(i);
+		}
 		_putchar('\n');
-	}
 	j++;
 	}
 }
