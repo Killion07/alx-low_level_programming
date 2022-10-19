effd75006122756ae22ba68ce34681baa70cdf2c
Killion07 <wkilion@gmail.com>
2022-10-19 10:02:50 -0700

    5 and 3 changes

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,17 +9,22 @@ int print_sign(int n)
 {
 
 	if (n > 0)
+	{
 		_putchar('+');
 	_putchar('\n');
 	return (1);
-
+	}
 	else if (n == 0)
+	{
 		_putchar('0');
 	_putchar('\n');
 	return (0);
+	}
 
 	else
+	{
 		_putchars('-');
 	_putchar('\n');
 	return (-1);
+	}
 }
