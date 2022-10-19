6f75aa2844fa27b637cacf1500ba6afc25faf480
Killion07 <wkilion@gmail.com>
2022-10-18 21:09:49 -0700

    4th assignments checks

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *_islower - checks if characters is lower
+ *
+ * Return: ret
+ */
+int _islower(int c)
+{
+	char ch;
+	int ret;
+
+	if (_islower(ch))
+		ret = 1;
+	else
+		ret = 0;
+
+return (ret);
+}
