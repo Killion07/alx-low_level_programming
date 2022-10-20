c013b17fcc140d207c69be94ebb95ea901a29ff4
Killion07 <wkilion@gmail.com>
2022-10-19 19:52:32 -0700

    9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,10 +12,11 @@ void times_table(void)
 	for (a = 0 ; a <= 9 ; a++)
 	{
 		_putchar('0');
-		_putchar(',');
-		_putchar(' ');
-		for (b = 0 ; b <= 9 ; b++)
+
+		for (b = 1 ; b <= 9 ; b++)
 		{
+			_putchar(',');
+			_putchar(' ');
 			c = b * a;
 
 			if (c <= 9)
