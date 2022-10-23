77c3af84c0874a616254061d9ac890e21dda2fcf
Killion07 <wkilion@gmail.com>
2022-10-22 19:13:10 -0700

    Function for printing numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - Function for printing numbers
+ */
+void print_numbers(void)
+{
+	int a = 0;
+
+	while (a <= 9)
+	{
+		_putchar(a);
+		a++;
+	}
+	_putchar('\n');
+
+}
