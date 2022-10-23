1678f0afb598d9c38caf203b1269ed8e0faaadf0
Killion07 <wkilion@gmail.com>
2022-10-23 15:11:39 -0700

    Print squares

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_square - Function fr printing square
+ * @size: value entered for the squares to be printed
+ */
+void print_square(int size)
+{
+	int a, b = 0;
+
+	if (size > 0)
+	{
+		while (b < size)
+		{
+			for (a = 0; a < size; a++)
+				_putchar(35);
+			_putchar('\n');
+
+		b++;
+		}	
+	}
+	else
+		_putchar('\n');
+}
