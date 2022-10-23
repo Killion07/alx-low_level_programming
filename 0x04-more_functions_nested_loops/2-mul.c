0d6901e16552977ffbc3052e9cef54e77605326f
Killion07 <wkilion@gmail.com>
2022-10-22 18:50:10 -0700

    Function for ,ultiplying

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - function for multiplying numbers
+ * @a: First integer to multiply
+ * @b: Second integer to multiply
+ * Return: Always return a 0
+ */
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+
+	return (0);
+}
