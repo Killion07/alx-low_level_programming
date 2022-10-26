1bc4b4ae2be094d77a72978ff8a524b3247659af
Killion07 <wkilion@gmail.com>
2022-10-25 19:21:59 -0700

    Functions for swapping integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * swap_int - function for swapping integers
+ * @a: 1st integer
+ * @b: 2nd integer
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = b;
+	*b = a;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,5 +2,8 @@
 #define _MAIN_H
 
 void reset_to_98(int *n);
+wap_int(int *a, int *b);
+
+#include <stdio.h>
 
 #endif
