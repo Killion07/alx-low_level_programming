3627306676730cf60731a0f35914da13d1fbb53f
Killion07 <wkilion@gmail.com>
2022-11-03 20:09:52 -0700

    Count of chars

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,12 +2,12 @@
 /**
  * _strlen_recursion - Function for calculating string lenght
  * @s: String to check length
+ * Return: Returns count of chars
  */
 int _strlen_recursion(char *s)
 {
 	if (s[0] != '\n')
-		return(1 + _strlen_recursion(s + 1));		
-	
-	else 
+		return (1 + _strlen_recursion(s + 1));
+	else
 		return (0);
 }
