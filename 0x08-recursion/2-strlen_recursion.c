3028831ef2268f33bf077a5234978fb4f6a973a1
Killion07 <wkilion@gmail.com>
2022-11-04 14:35:57 -0700

    Reccursions

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,10 +6,13 @@
  */
 int _strlen_recursion(char *s)
 {
-	if (s[0] != '\n')
+	int i = 0;
+
+	if (*s)
 	{
-		return (1 + _strlen_recursion(s + 1));
+		i++;
+		i += _strlen_recursion(s + 1);
 	}
-	else
-		return (0);
+
+	return (i);
 }
