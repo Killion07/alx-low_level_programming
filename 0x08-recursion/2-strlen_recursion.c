a28fd7741c47d90af98111f959dc3a8d479a65dc
Killion07 <wkilion@gmail.com>
2022-11-03 20:05:35 -0700

    Counting strings

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strlen_recursion - Function for calculating string lenght
+ * @s: String to check length
+ */
+int _strlen_recursion(char *s)
+{
+	if (s[0] != '\n')
+		return(1 + _strlen_recursion(s + 1));		
+	
+	else 
+		return (0);
+}
