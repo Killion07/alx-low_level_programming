d8f0a5e4ad04a572174281948b51ace8609a53ec
Killion07 <wkilion@gmail.com>
2022-11-04 04:50:41 -0700

    Power calculator

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -3,6 +3,7 @@
  * _pow_recursion - Function for pow calculation
  * @x: Base
  * @y: Power form
+ * Return: Returns the calculated value
  */
 int _pow_recursion(int x, int y)
 {
@@ -13,5 +14,5 @@ int _pow_recursion(int x, int y)
 		return (1);
 
 	else
-		return (x * _pow_recursion(x, y-1));
-}	
+		return (x * _pow_recursion(x, y - 1));
+}
