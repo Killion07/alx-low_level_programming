fd0a2a3dae72d23377a1efaa152448b63e51eec6
Killion07 <wkilion@gmail.com>
2022-11-04 01:06:38 -0700

    Factorial claculator

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -7,7 +7,9 @@
 int _strlen_recursion(char *s)
 {
 	if (s[0] != '\n')
+	{
 		return (1 + _strlen_recursion(s + 1));
+	}
 	else
 		return (0);
 }
---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * factorial - Function for running factorial
+ * @n: Number given
+ * Return: Return facrorial claculation else -1
+ */
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+
+	else if (n == 0)
+		return (1);
+
+	else
+		return (n * factorial(n -1));
+}
