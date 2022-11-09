9ef870e466a4ada75673c2ec756e0fdc6bdf6297
Killion07 <wkilion@gmail.com>
2022-11-09 12:40:56 -0800

    Strings copy

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib>
+/**
+ * _strdup - Fuction for coppying
+ * @str: String to be copied
+ * Return: Nul or string
+ */
+
+char *_strdup(char *str)
+{
+	char *c;
+	int i;
+	int len = 0;
+
+	if (str == 0)
+		return (NULL);
+
+	while (str[len] != '\0')
+		len++;
+
+	c = (char *)malloc((sizeof(char) * len) + 1);
+	if (c == NULL)
+		return (NULL);
+
+	for (i = 0; i < len; i++)
+		c[i] = str[i];
+	c[len] = '\0';
+
+	return (c);
+}
+
