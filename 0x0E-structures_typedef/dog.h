6de7facb689a7932e1224d330c4d77c16d137081
Killion07 <wkilion@gmail.com>
2022-11-15 14:19:13 -0800

    Type def

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * struct dog - Dog data
+ * @name: Name of the doge
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} my_dog;
+
+#endif
