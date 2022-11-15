c87cd6798cd63817d2e680bb1a91f3b6bfae0862
Killion07 <wkilion@gmail.com>
2022-11-15 14:42:08 -0800

    tast 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,6 +1,10 @@
 #include "dog.h"
 /**
- *
+ * init_dog - function for printing dog details
+ * @d: name of struct
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
