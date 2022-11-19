3ded83017b00bd433dac8ec553dc81db082f76bf
Killion07 <wkilion@gmail.com>
2022-11-19 12:15:46 -0800

    Sum them all

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,24 @@
+#include <stdarg.h>
+/**
+ * sum_them_all - Function for addition
+ * @n: Number of integers added
+ * Return: Returns 0 or summ
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	int sum = 0;
+	va_list a;
+
+	va_start(a, n);
+
+	if (n == 0)
+		return (0);
+
+	for (i = 0; i < n; i++)
+		sum += va_arg(a, unsigned int);
+
+	va_end(a);
+
+	return (sum);
+}
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,6 @@
+#ifndef _VF_
+#define _VF_
+
+int sum_them_all(const unsigned int n, ...);
+
+#endif
