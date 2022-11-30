aa2cd2a2bb9b6b78aa60f49af00868cd43a6aeaf
Killion07 <wkilion@gmail.com>
2022-11-29 17:42:48 -0800

    String lenght

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * list_len - Function for retruning number of linked lists
+ * @h: Number of nodes
+ * Return: Number of linked list
+ */
+
+size_t list_len(const list_t *h)
+{
+	int count = 0;
+	
+	while (h)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
